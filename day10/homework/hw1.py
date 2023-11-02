d34e8cc956faca5fd50602d9f99f40358a755d76
giorgilobjanidze1996gmailcom <144389749+giorgilobjanidze1996gmailcom@users.noreply.github.com>
2023-11-02 04:40:20 +0400

    Add files via upload

---
+++ day10/homework/hw1.py
@@ -0,0 +1,12 @@
+import random
+students=["tsotne","giorgi","tinatini","salome","gabrieli","nika"]
+for i in range (len(students)):
+        print("mopasuxe aris")
+        print(students[random.randint(0,5)])
+        mopasuxe=random.choice(students)
+        answer=(input("mopasuxem gasca sworad pasuxi? :"))
+        if answer=="ki":
+                print("mopasuxe+1 qula")
+        elif answer=="ara":
+                print("mopasuxem chaagdo es kitxva")
+students.remove(mopasuxe)
