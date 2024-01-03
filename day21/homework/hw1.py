d41c8e68bba17d0fc37d8a140c573c69cc3ddf05
giorgilobjanidze1996gmailcom <144389749+giorgilobjanidze1996gmailcom@users.noreply.github.com>
2024-01-03 21:19:21 +0400

    Add files via upload

---
+++ day21/homework/hw1.py
@@ -0,0 +1,10 @@
+# drink about
+def people_with_age_drink(age):
+    if age < 14:
+        return "drink toddy"
+    elif age >= 14 and age < 18:
+        return "drink coke"
+    elif age >= 18 and age < 21:
+        return "drink beer"
+    elif age >= 21:
+        return "drink whisky"
